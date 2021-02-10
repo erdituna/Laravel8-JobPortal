297cea8640be0ff2bc16ed7078201c37dd15d7d5
erdituna <erdituna36@gmail.com>
2021-02-10 16:00:25 +0300

    2.düzenleme yapıldı

--- resources/views/home/_category.blade.php
+++ resources/views/home/_category.blade.php
@@ -45,14 +45,5 @@
 
 
 
-        <!-- More Btn -->
-        <!-- Section Button -->
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="browse-btn2 text-center mt-50">
-                    <a href="job_listing.html" class="border-btn2">Browse All Sectors</a>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
